06d7a3b9fab03d1c46f35394b7339a276061a84f
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-04-12 23:27:26 +0500

    switch enabled

--- redux-starter/src/reducer.js
+++ redux-starter/src/reducer.js
@@ -2,20 +2,44 @@ const initialValue = [];
 let lastId = 0;
 
 export default function reducer(state = initialValue, action) {
-  if (action.type === "bugAdded") {
-    return [
-      ...state,
-      {
-        id: ++lastId,
-        description: action.payload.description,
-        resolved: false,
-      },
-    ];
-  } else if (action.type === "bugRemoved") {
-    return state.filter((bug) => bug.id !== action.payload.id);
-  } else if (action.type === "bugResolved") {
-    return state.map((bug) =>
-      bug.id === action.payload.id ? { ...bug, resolved: true } : bug
-    );
+  switch (action.type) {
+    case "bugAdded":
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+
+    case "bugRemoved":
+      return state.filter((bug) => bug.id !== action.payload.id);
+
+    case "bugResolved":
+      return state.map((bug) =>
+        bug.id === action.payload.id ? { ...bug, resolved: true } : bug
+      );
+
+    default:
+      return state;
   }
 }
+// export default function reducer(state = initialValue, action) {
+//   if (action.type === "bugAdded") {
+//     return [
+//       ...state,
+//       {
+//         id: ++lastId,
+//         description: action.payload.description,
+//         resolved: false,
+//       },
+//     ];
+//   } else if (action.type === "bugRemoved") {
+//     return state.filter((bug) => bug.id !== action.payload.id);
+//   } else if (action.type === "bugResolved") {
+//     return state.map((bug) =>
+//       bug.id === action.payload.id ? { ...bug, resolved: true } : bug
+//     );
+//   }
+// }
