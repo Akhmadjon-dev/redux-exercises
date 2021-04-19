100f2cbd954b83114959e05e8150966e6d8ad8cc
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-04-19 23:43:37 +0500

    toolkit added

--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -1,11 +1,13 @@
 import { createStore } from "redux";
 import { devToolsEnhancer } from "redux-devtools-extension";
 import reducer from "./bug";
+import { configureStore } from "@reduxjs/toolkit";
 
-const store = createStore(
-  reducer,
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  devToolsEnhancer({ trace: true })
-);
+const store = configureStore({ reducer });
+// const store = createStore(
+//   reducer,
+//   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   devToolsEnhancer({ trace: true })
+// );
 
 export default store;
